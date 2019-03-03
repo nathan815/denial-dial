fc8789e374d117028b65d2a706a8a3075968ad38
Nathaniel Johnson <nathancj224@gmail.com>
2019-03-03 12:34:38 -0500

    add DB_HOST env variable

--- app/db/connect.js
+++ app/db/connect.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const { DB_NAME, DB_PORT, DB_USER, DB_PASS } = process.env;
+const { DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASS } = process.env;
 try {
-    mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@localhost:${DB_PORT}/${DB_NAME}`,
+    mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
     { useNewUrlParser: true });
 } catch(err) {
     console.error('Unable to connect to database!', err);
