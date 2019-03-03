0b919138ae4c12d33d2ce73d53491a471f8c4ac8
Nathaniel Johnson <nathancj224@gmail.com>
2019-03-03 11:29:36 -0500

    combine twilio service into message service

--- app/services/message.js
+++ app/services/message.js
@@ -1,4 +1,7 @@
+const { TWILIO_ID, TWILIO_TOKEN } = process.env;
 const { Message } = require('../db/models');
+const twilio = require('twilio');
+const client = twilio(TWILIO_ID, TWILIO_TOKEN);
 
 module.exports = {
     async createMessage(message, checkIfExists = false) {
@@ -10,11 +13,31 @@ module.exports = {
                 return false;
             }
         }
-        console.log('Creating message record', message)
+        console.log('Creating message document')
         return Message.create({
             id: message.sid,
             from: message.from,
             body: message.body
         });
+    },
+
+    async sendFinalMessage(number) {
+        console.log('send final message');
+        return await this.send({
+            to: number,
+            body: "Rejection Hotline!"
+        });
+    },
+
+    /**
+     * Sends a message
+     * 
+     * @param {object} message { body: String, to: String }
+     */
+    async send(message) {
+        const fromNumber = process.env.TWILIO_NUMBER;
+        message['from'] = fromNumber;
+        const message = await client.messages.create(message);
+        return this.createMessage(message);
     },
 }
